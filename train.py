5e452871755965073cba4ecb7c18220c100b42a0
Will <will.hartley@epimorphics.com>
2020-05-27 16:12:57 +0100

    CPU Mode

--- train.py
+++ train.py
@@ -48,7 +48,8 @@ if __name__ == '__main__':
 
         # for i, data in enumerate(dataset):
         for i, data_raw in enumerate(dataset_loader):
-            data_raw[0] = data_raw[0].cuda()
+            if len(opt.gpu_ids) > 0:
+                data_raw[0] = data_raw[0].cuda()
             data = util.get_colorization_data(data_raw, opt, p=opt.sample_p)
             if(data is None):
                 continue
