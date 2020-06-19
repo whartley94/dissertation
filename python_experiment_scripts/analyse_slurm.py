688cbd5c7906def939da3bae1192a4ca2e9e92fa
Will <will.hartley@epimorphics.com>
2020-06-19 12:27:57 +0100

    Refactor slurm analysis

--- python_experiment_scripts/analyse_slurm.py
+++ python_experiment_scripts/analyse_slurm.py
@@ -7,220 +7,232 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import re
 
-slurm_log_dir = '/Users/Will/Documents/Uni/MscEdinburgh/Diss/slurm_logs'
-slurm_dirs = []
-slurm_exp = '947669'
-for i in os.listdir(slurm_log_dir):
-    if '.out' in i and slurm_exp in i:
-        slurm_dirs.append(i)
+def get_logs(slurm_exp):
+    slurm_dirs = []
+    for i in os.listdir(slurm_log_dir):
+        if '.out' in i and slurm_exp in i:
+            slurm_dirs.append(i)
+    return slurm_dirs
 
-time_epoch_pre = []
-count_listdir = 0
 
-for i in slurm_dirs:
-        count_listdir += 1
-        f = open(os.path.join(slurm_log_dir, i), "r")
-
-        for j in f:
-            if 'End of epoch' in j:
+def get_epoch_times_variable_length(slurm_dirs):
+    time_epoch_pre = []
+    for i in slurm_dirs:  # Loop over the relevant out files.
+        file = open(os.path.join(slurm_log_dir, i), "r")
+        for line in file:  # Loop over the lines of each out file.
+            # Find .out files which some epochs finished, i.e containing 'End of epoch'.
+            if 'End of epoch' in line:
+                # Example: End of epoch 3 / 5 	 Time Taken: 1509 sec --- We want the numbers, 3,5 and 1509 in our array.
                 words = []
-                for word in j.split():
-                    if word.isdigit():
+                for word in line.split():
+                    if word.isdigit():  # Get just the numbers we want form the line.
                         words.append(word)
                 assert len(words) == 3
                 time_epoch_pre.append(words)
-
-time_epoch_pre = np.asarray(time_epoch_pre).astype(int)
-max_epochs = np.max(time_epoch_pre, axis=0)[1]
-time_epoch = np.empty((count_listdir, max_epochs, 3))
-time_epoch[:] = np.nan
-
-name_arr = []
-batch_size_arr = []
-gpu_arr = []
-phase_arr = []
-dict_arrs = []
-
-slurm_num = 0
-for i in slurm_dirs:
-    f = open(os.path.join(slurm_log_dir, i), "r")
-    epoch = 0
-    dict_arr = []
-
-    for k in f:
-
-        if 'Running provided' in k:
-            argString = str(k[42:])
-            opt = TrainOptions().gather_options2(shlex.split(argString))
-
-            batch_size_arr.append(opt.batch_size)
-            gpu_arr.append(opt.gpu_ids)
-            phase_arr.append(opt.phase)
-
-        if 'End of epoch' in k:
-            words = []
-            for word in k.split():
-                if word.isdigit():
-                    words.append(word)
-            assert len(words) == 3
-            time_epoch[slurm_num, epoch, :] = words
-            epoch += 1
-
-        if 'Experiment Name' in k:
-            name_arr.append(str(k[18:-1]))
-
-        if '(epoch: ' in k:
-            splits = k[1:].split(',')
-            splits_dic = {}
-            for r in splits[:-2]:
-                res = re.split(': |\) ', r)
-                for m in range(0, len(res), 2):
-                    # print(m)
-                    splits_dic[res[m]]  = res[m+1]
-            dict_arr.append(splits_dic)
-
-    slurm_num += 1
-    dict_arrs.append(dict_arr)
+    time_epoch_pre = np.asarray(time_epoch_pre).astype(int)
+    return time_epoch_pre
 
 
+def populate_data_arrays():
+    name_arr, batch_size_arr, gpu_arr, phase_arr, dict_arrs = [], [], [], [], []
 
-total_dict_arrs = len(dict_arrs)
-g_ce_arr = np.empty(total_dict_arrs)
-g_ce_arr[:] = np.nan
-num_dict_exp = np.empty(total_dict_arrs)
-for i in range(total_dict_arrs):
-    len_dicts = len(dict_arrs[i])
-    num_dict_exp[i] = len_dicts
-    if len_dicts-1 >= 0:
-        if int(dict_arrs[i][len_dicts-1]['epoch']) == int(max_epochs)-1:
-            g_ce_arr[i] = dict_arrs[i][len_dicts-1][' G_L1_reg']
+    for slurm_num, i in enumerate(slurm_dirs):  # Loop over .out files.
+        file = open(os.path.join(slurm_log_dir, i), "r")
+        epoch = 0
+        dict_arr = []
 
-    # print(dict_arrs[i][len_dicts])
+        for line in file:
 
-# print(dict_arrs[12][300])
+            if 'Running provided' in line:  # Gets the command used to run that particular .out file.
+                arg_string = str(line[42:])
+                # Parse the command args as if we were actually calling it, for the dictionary.
+                opt = TrainOptions().gather_options2(shlex.split(arg_string))
 
-assert len(name_arr) == len(batch_size_arr)
-assert len(name_arr) == len(time_epoch)
+                batch_size_arr.append(opt.batch_size)
+                gpu_arr.append(opt.gpu_ids)
+                phase_arr.append(opt.phase)
 
-batch_size_arr = np.asarray(batch_size_arr)
-gpu_arr = np.asarray(gpu_arr)
-phase_arr = np.asarray(phase_arr)
+            if 'End of epoch' in line:  # Gets info from lines with the epoch number and how long it took.
+                words = []
+                for word in line.split():
+                    if word.isdigit():
+                        words.append(word)
+                assert len(words) == 3
+                time_epoch[slurm_num, epoch, :] = words
+                epoch += 1
 
-time_epoch = np.asarray(time_epoch).astype(float)
-gpu_arr = np.asarray([len(gpu_arr[i].split(',')) for i in range(len(gpu_arr))])
-phase_num = [len(i) for i in phase_arr]
-times = np.mean(time_epoch[:, :, 2], axis=1)
+            if 'Experiment Name' in line:  # Gets the experiment name.
+                name_arr.append(str(line[18:-1]))
 
-fails = [np.isnan(i) for i in times]
-g_ce_arr[fails] = np.nan
+            if '(epoch: ' in line:  # This is getting the desired losses from rows in which it was printed.
+                splits = line[1:].split(',')
+                splits_dic = {}
+                for r in splits[:-2]:
+                    res = re.split(': |\) ', r)
+                    for m in range(0, len(res), 2):
+                        # print(m)
+                        splits_dic[res[m]] = res[m + 1]
+                dict_arr.append(splits_dic)
 
-trains = [i == 'train' for i in phase_arr]
-train_smalls = [i == 'train_small' for i in phase_arr]
+        dict_arrs.append(dict_arr)
 
-batch_size_arr_trains = batch_size_arr[trains]
-g_ce_arr_trains = g_ce_arr[trains]
-gpu_arr_trains = gpu_arr[trains]
+    batch_size_arr = np.asarray(batch_size_arr)
+    gpu_arr = np.asarray(gpu_arr)
+    phase_arr = np.asarray(phase_arr)
+    gpu_arr = np.asarray([len(gpu_arr[i].split(',')) for i in range(len(gpu_arr))])
 
-bs10 = [i == 10 for i in batch_size_arr_trains]
-bs25 = [i == 25 for i in batch_size_arr_trains]
-bs32 = [i == 32 for i in batch_size_arr_trains]
-bs64 = [i == 64 for i in batch_size_arr_trains]
-bs128 = [i == 128 for i in batch_size_arr_trains]
+    assert len(name_arr) == len(batch_size_arr)
 
-print(g_ce_arr_trains[bs10])
-print(gpu_arr_trains[bs10])
-
-fig = plt.figure(figsize=(10, 5))
-ax = fig.add_subplot(111)
-
-new_x, new_y = zip(*sorted(zip(gpu_arr_trains[bs10], g_ce_arr_trains[bs10])))
-ax.plot(new_x, new_y, label='Batch Size 10')
-new_x, new_y = zip(*sorted(zip(gpu_arr_trains[bs25], g_ce_arr_trains[bs25])))
-ax.plot(new_x, new_y, label='Batch Size 25')
-new_x, new_y = zip(*sorted(zip(gpu_arr_trains[bs32], g_ce_arr_trains[bs32])))
-ax.plot(new_x, new_y, label='Batch Size 32')
-new_x, new_y = zip(*sorted(zip(gpu_arr_trains[bs64], g_ce_arr_trains[bs64])))
-ax.plot(new_x, new_y, label='Batch Size 64')
-new_x, new_y = zip(*sorted(zip(gpu_arr_trains[bs128], g_ce_arr_trains[bs128])))
-ax.plot(new_x, new_y, label='Batch Size 128')
-# ax.scatter(gpu_arr[bs25], g_ce_arr[bs25], label='Batch Size 25')
-# ax.scatter(gpu_arr[bs32], g_ce_arr[bs32], label='Batch Size 32')
-# ax.scatter(gpu_arr[bs64], g_ce_arr[bs64], label='Batch Size 64')
-# ax.scatter(gpu_arr[bs128], g_ce_arr[bs128], label='Batch Size 128')
-# ax.scatter(gpu_arr[train_smalls], times[train_smalls], label='Train Small')
-ax.set_xlabel('Number of GPUs')
-ax.set_ylabel('Loss After 5 Epochs')
-ax.legend(bbox_to_anchor=(1.04, 1), loc="upper left")
-plt.subplots_adjust(right=0.7)
-plt.show()
-# plt.savefig(self.mpl_name, bbox_inches="tight", dpi=400)
-plt.close(fig)
+    return name_arr, batch_size_arr, gpu_arr, phase_arr, dict_arrs
 
 
+def get_final_losses_array(dict_arrs):
+    total_dict_arrs = len(dict_arrs)
+    g_ce_arr = np.empty(total_dict_arrs)
+    g_ce_arr[:] = np.nan
+    num_dict_exp = np.empty(total_dict_arrs)
+    for i in range(total_dict_arrs):
+        len_dicts = len(dict_arrs[i])
+        num_dict_exp[i] = len_dicts
+        if len_dicts - 1 >= 0:
+            if int(dict_arrs[i][len_dicts - 1]['epoch']) == int(max_epochs) - 1:
+                g_ce_arr[i] = dict_arrs[i][len_dicts - 1][' G_L1_reg']
 
-# fig = plt.figure(figsize=(10, 5))
-# ax = fig.add_subplot(111)
-# # ax.set_title(self.name + ' loss over time')
-# ax.scatter(batch_size_arr[trains], times[trains], label='Train')
-# # ax.scatter(batch_size_arr[train_smalls], times[train_smalls], label='Train Small')
-# ax.set_xlabel('Batch Size')
-# ax.set_ylabel('Mean Epoch Time Taken')
-# ax.legend(bbox_to_anchor=(1.04, 1), loc="upper left")
-# plt.subplots_adjust(right=0.7)
-# plt.show()
-# # plt.savefig(self.mpl_name, bbox_inches="tight", dpi=400)
-# plt.close(fig)
-# #
-#
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# ax.scatter(batch_size_arr[trains], gpu_arr[trains], times[trains])
-# ax.set_xlabel('Batch Size')
-# ax.set_ylabel('Number of GPUs')
-# ax.set_zlabel('Mean Time Taken (One Epoch)')
-# # plt.show()
-#
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# ax.scatter(batch_size_arr[train_smalls], gpu_arr[train_smalls], times[train_smalls])
-# ax.set_xlabel('Batch Size')
-# ax.set_ylabel('Number of GPUs')
-# ax.set_zlabel('Mean Time Taken (One Epoch)')
-# # plt.show()
+    return g_ce_arr, num_dict_exp
 
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# ax.scatter(batch_size_arr[trains], gpu_arr[trains], g_ce_arr[trains])
-# ax.set_xlabel('Batch Size')
-# ax.set_ylabel('Number of GPUs')
-# ax.set_zlabel('Loss After 5 Epochs')
-# plt.show()
 
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# ax.scatter(batch_size_arr[train_smalls], gpu_arr[train_smalls], g_ce_arr[train_smalls])
-# ax.set_xlabel('Batch Size')
-# ax.set_ylabel('Number of GPUs')
-# ax.set_zlabel('Loss After 5 Epochs')
-# plt.show()
+def zip_plot(gpu_arr_trains, g_ce_arr_trains, ax, bs, bs_num):
+    new_x, new_y = zip(*sorted(zip(gpu_arr_trains[bs], g_ce_arr_trains[bs])))
+    ax.plot(new_x, new_y, label='Batch Size ' + str(bs_num))
 
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-#
-# # normalized1 = (g_ce_arr[trains]-np.nanmin(g_ce_arr[trains]))/(np.nanmax(g_ce_arr[trains])-np.nanmin(g_ce_arr[trains]))
-# # normalized2 = (times[trains]-np.nanmin(times[trains]))/(np.nanmax(times[trains])-np.nanmin(times[trains]))
-#
-# # normalized3 = 1 / (1.927 - g_ce_arr[trains])
-# trains_or_smalls = trains
-#
-# normalized3 =  g_ce_arr[trains_or_smalls]/np.nanmax(g_ce_arr[trains_or_smalls])
-# normalized4 = times[trains_or_smalls]/np.nanmax(times[trains_or_smalls])
-#
-# mulp = g_ce_arr[trains_or_smalls] * times[trains_or_smalls]
-# mulp2 = normalized3 * normalized4
-# # print(mulp)
-# ax.scatter(batch_size_arr[trains_or_smalls], gpu_arr[trains_or_smalls], mulp2)
-# ax.set_xlabel('Batch Size')
-# ax.set_ylabel('Number of GPUs')
-# ax.set_zlabel('\'Ratio\' Loss & Time Taken')
-# plt.show()
+
+def loss_vs_gpu_plot():
+    # Split line out foe each batch size used.
+    batch_size_numbers = [10, 25, 32, 64, 128]
+    batch_size_lines = []
+    for count, bs in enumerate(batch_size_numbers):
+        bs = [i == bs for i in batch_size_arr_trains]
+        batch_size_lines.append(bs)
+
+    fig = plt.figure(figsize=(10, 5))
+    ax = fig.add_subplot(111)
+
+    # Plot each line (one per batch size).
+    for line, bs in enumerate(batch_size_lines):
+        # Zips and sorts so that the number of GPUs is in order.
+        zip_plot(gpu_arr_trains, g_ce_arr_trains, ax, bs, batch_size_numbers[line])
+
+    # ax.scatter(gpu_arr[bs25], g_ce_arr[bs25], label='Batch Size 25')
+    # ax.scatter(gpu_arr[bs32], g_ce_arr[bs32], label='Batch Size 32')
+    # ax.scatter(gpu_arr[bs64], g_ce_arr[bs64], label='Batch Size 64')
+    # ax.scatter(gpu_arr[bs128], g_ce_arr[bs128], label='Batch Size 128')
+    # ax.scatter(gpu_arr[train_smalls], times[train_smalls], label='Train Small')
+    ax.set_xlabel('Number of GPUs')
+    ax.set_ylabel('Loss After 5 Epochs')
+    ax.legend(bbox_to_anchor=(1.04, 1), loc="upper left")
+    plt.subplots_adjust(right=0.7)
+    plt.show()
+    # plt.savefig(self.mpl_name, bbox_inches="tight", dpi=400)
+    plt.close(fig)
+
+
+if __name__ == '__main__':
+
+    slurm_log_dir = '/Users/Will/Documents/Uni/MscEdinburgh/Diss/slurm_logs'
+    slurm_dirs = get_logs(slurm_exp='947669')
+
+    # Get the epoch times but without worrying about producing a numpy array with fixed shape.
+    time_epoch_pre = get_epoch_times_variable_length(slurm_dirs)
+
+    max_epochs = np.max(time_epoch_pre, axis=0)[1]  # Get max number of epochs completed across any of the .out files.
+    # Initialise a numpy array of fixed shape.
+    time_epoch = np.empty((len(slurm_dirs), max_epochs, 3))
+    time_epoch[:] = np.nan
+    time_epoch = np.asarray(time_epoch).astype(float)
+
+    # Dict_arrs is a list of lists of dictionaries of the loss printouts for each experiment.
+    name_arr, batch_size_arr, gpu_arr, phase_arr, dict_arrs = populate_data_arrays()
+    assert len(name_arr) == len(time_epoch)
+    g_ce_arr, num_dict_exp = get_final_losses_array(dict_arrs)  # Get final loss for each experiment
+
+    # Prepare data for plotting
+    phase_num = [len(i) for i in phase_arr]  # Convert phase ('train'/'train_small') to an integer for plotting.
+    times = np.mean(time_epoch[:, :, 2], axis=1)  # Find the average time per epoch for each experiment.
+    fails = [np.isnan(i) for i in times]
+    g_ce_arr[fails] = np.nan  # Zero out any losses for epochs which didn't complete.
+    trains = [i == 'train' for i in phase_arr]  # Indexes of .out files with 'train' experiments.
+    train_smalls = [i == 'train_small' for i in phase_arr]  # Indexes of .out files with 'train_small' experiments.
+    batch_size_arr_trains = batch_size_arr[trains]
+    g_ce_arr_trains = g_ce_arr[trains]
+    gpu_arr_trains = gpu_arr[trains]
+
+    # Do plotting
+    loss_vs_gpu_plot()
+
+
+    # fig = plt.figure(figsize=(10, 5))
+    # ax = fig.add_subplot(111)
+    # # ax.set_title(self.name + ' loss over time')
+    # ax.scatter(batch_size_arr[trains], times[trains], label='Train')
+    # # ax.scatter(batch_size_arr[train_smalls], times[train_smalls], label='Train Small')
+    # ax.set_xlabel('Batch Size')
+    # ax.set_ylabel('Mean Epoch Time Taken')
+    # ax.legend(bbox_to_anchor=(1.04, 1), loc="upper left")
+    # plt.subplots_adjust(right=0.7)
+    # plt.show()
+    # # plt.savefig(self.mpl_name, bbox_inches="tight", dpi=400)
+    # plt.close(fig)
+    # #
+    #
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    # ax.scatter(batch_size_arr[trains], gpu_arr[trains], times[trains])
+    # ax.set_xlabel('Batch Size')
+    # ax.set_ylabel('Number of GPUs')
+    # ax.set_zlabel('Mean Time Taken (One Epoch)')
+    # # plt.show()
+    #
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    # ax.scatter(batch_size_arr[train_smalls], gpu_arr[train_smalls], times[train_smalls])
+    # ax.set_xlabel('Batch Size')
+    # ax.set_ylabel('Number of GPUs')
+    # ax.set_zlabel('Mean Time Taken (One Epoch)')
+    # # plt.show()
+
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    # ax.scatter(batch_size_arr[trains], gpu_arr[trains], g_ce_arr[trains])
+    # ax.set_xlabel('Batch Size')
+    # ax.set_ylabel('Number of GPUs')
+    # ax.set_zlabel('Loss After 5 Epochs')
+    # plt.show()
+
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    # ax.scatter(batch_size_arr[train_smalls], gpu_arr[train_smalls], g_ce_arr[train_smalls])
+    # ax.set_xlabel('Batch Size')
+    # ax.set_ylabel('Number of GPUs')
+    # ax.set_zlabel('Loss After 5 Epochs')
+    # plt.show()
+
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    #
+    # # normalized1 = (g_ce_arr[trains]-np.nanmin(g_ce_arr[trains]))/(np.nanmax(g_ce_arr[trains])-np.nanmin(g_ce_arr[trains]))
+    # # normalized2 = (times[trains]-np.nanmin(times[trains]))/(np.nanmax(times[trains])-np.nanmin(times[trains]))
+    #
+    # # normalized3 = 1 / (1.927 - g_ce_arr[trains])
+    # trains_or_smalls = trains
+    #
+    # normalized3 =  g_ce_arr[trains_or_smalls]/np.nanmax(g_ce_arr[trains_or_smalls])
+    # normalized4 = times[trains_or_smalls]/np.nanmax(times[trains_or_smalls])
+    #
+    # mulp = g_ce_arr[trains_or_smalls] * times[trains_or_smalls]
+    # mulp2 = normalized3 * normalized4
+    # # print(mulp)
+    # ax.scatter(batch_size_arr[trains_or_smalls], gpu_arr[trains_or_smalls], mulp2)
+    # ax.set_xlabel('Batch Size')
+    # ax.set_ylabel('Number of GPUs')
+    # ax.set_zlabel('\'Ratio\' Loss & Time Taken')
+    # plt.show()
