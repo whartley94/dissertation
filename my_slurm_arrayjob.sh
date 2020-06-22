45f1967c6139248924e5f96e2ccdfbcf16ce9e5c
whartley94 <s1843503@uhtred.inf.ed.ac.uk>
2020-06-22 11:06:56 +0100

    slurm array job

--- my_slurm_arrayjob.sh
+++ my_slurm_arrayjob.sh
@@ -48,7 +48,7 @@
 #SBATCH --cpus-per-task=4
 
 # Maximum time for the job to run, format: days-hours:minutes:seconds
-# #SBATCH --time=1-04:00:00
+# #SBATCH --time=1-08:00:00
 
 # Partition of the cluster to pick nodes from (check `sinfo`)
 #SBATCH --partition=Teach-Standard
