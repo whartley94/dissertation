05271dbf4dbff9db7ac02f311ad2c02187aade53
whartley94 <s1843503@uhtred.inf.ed.ac.uk>
2020-06-23 10:01:12 +0100

    Modified experiment gen script

--- python_experiment_scripts/gen_experiment.py
+++ python_experiment_scripts/gen_experiment.py
@@ -13,11 +13,11 @@ cpoint_path = f'{SCRATCH_HOME}/checkpoints'
 
 # base_call = f"python3 train.py -i {DATA_HOME}/input -o {DATA_HOME}/output"
 
-base_call = f"python train.py --name Auto --sample_p 1.0 --niter 20 --niter_decay 0 --classification --display_id -1 " \
-    f"--data_dir {sorted_path} --checkpoints_dir {cpoint_path} --phase train_small"
+base_call = f"python train.py --name Auto --sample_p 1.0 --niter 50 --niter_decay 0 --classification --display_id -1 " \
+    f"--data_dir {sorted_path} --checkpoints_dir {cpoint_path} --phase train"
 
-base_call2 = f"python train.py --name Auto --sample_p .125 --niter 20 --niter_decay 0 --lr 0.000001 --display_id -1 " \
-    f"--data_dir {sorted_path} --checkpoints_dir {cpoint_path} --phase train --load_sg_model"
+# base_call2 = f"python train.py --name Auto --sample_p .125 --niter 20 --niter_decay 0 --lr 0.000001 --display_id -1 " \
+#    f"--data_dir {sorted_path} --checkpoints_dir {cpoint_path} --phase train --load_sg_model"
 
 # train.py --name siggraph_reg2 --sample_p .125 --niter 20 --niter_decay 0 --lr 0.000001 --load_model --phase train
 
@@ -26,8 +26,8 @@ base_call2 = f"python train.py --name Auto --sample_p .125 --niter 20 --niter_de
 # weight_decays = [1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1]
 
 components = ['batch_size', 'gpu_ids']
-batch_sizes = [32, 64, 98, 128]
-gpu_idss = ['0,1,2,3', '0,1,2,3,4,5,6,7']
+batch_sizes = [10, 25, 32, 64, 98, 128]
+gpu_idss = ['0', '0,1', '0,1,2,3', '0,1,2,3,4,5,6,7']
 # num_threads = [0, 4]
 # phases = ['train_small', 'train']
 flattened_list = list(itertools.product(batch_sizes, gpu_idss))
@@ -45,11 +45,11 @@ for i in flattened_list:
         expt_call += f"--" + components[j] + f" {i[j]} "
     print(expt_call, file=output_file)
 
-for i in flattened_list:
-    expt_call = f"{base_call2} "
-    for j in range(len(i)):
-        expt_call += f"--" + components[j] + f" {i[j]} "
-    print(expt_call, file=output_file)
+#for i in flattened_list:
+#    expt_call = f"{base_call2} "
+#    for j in range(len(i)):
+#        expt_call += f"--" + components[j] + f" {i[j]} "
+#    print(expt_call, file=output_file)
 
 # for batch_size, gpu_ids, phase in flattened_list:
 #     expt_call = (
