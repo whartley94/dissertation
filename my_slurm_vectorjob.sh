21cebad66bbaf8678f1238c5058e6cc6ea14346e
Will <will.hartley@epimorphics.com>
2020-06-23 10:13:43 +0100

    Vectorjob test setup

--- my_slurm_vectorjob.sh
+++ my_slurm_vectorjob.sh
@@ -60,7 +60,7 @@
 # =====================
 # Logging information
 # =====================
-MODEL_NAME=vectorjob
+MODEL_NAME=vectorjobtest 
 
 # slurm info - more at https://slurm.schedmd.com/sbatch.html#lbAJ
 echo "Job running on ${SLURM_JOB_NODELIST}"
