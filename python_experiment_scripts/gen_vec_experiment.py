e7c18efeac8b717a702c0458de20391003df1ac6
whartley94 <s1843503@uhtred.inf.ed.ac.uk>
2020-06-24 17:04:38 +0100

    Vec experinment script

--- python_experiment_scripts/gen_vec_experiment.py
+++ python_experiment_scripts/gen_vec_experiment.py
@@ -14,7 +14,7 @@ cpoint_path = f'{SCRATCH_HOME}/checkpoints'
 # base_call = f"python3 train.py -i {DATA_HOME}/input -o {DATA_HOME}/output"
 
 base_call = f"python train.py --sample_p 1.0 --niter_decay 0 --classification --display_id -1 " \
-    f"--data_dir {sorted_path} --checkpoints_dir {cpoint_path} --phase train " \
+    f"--data_dir {sorted_path} --checkpoints_dir {cpoint_path} --phase train_small " \
     f"--batch_size 32 --gpu_ids 0,1,2,3,4,5,6,7"
 
 # base_call2 = f"python train.py --name Auto --sample_p .125 --niter 20 --niter_decay 0 --lr 0.000001 --display_id -1 " \
@@ -27,8 +27,8 @@ base_call = f"python train.py --sample_p 1.0 --niter_decay 0 --classification --
 # weight_decays = [1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1]
 
 components = ['niter', 'epoch_count']
-niters = [10, 20, 30, 40, 50]
-epoch_counts = [0, 10, 20, 30, 40]
+niters = [2, 4, 6, 8, 10]
+epoch_counts = [0, 2, 4, 6, 8]
 flattened_list  = list(zip(niters, epoch_counts))
 print(flattened_list)
 # batch_sizes = [32, 64, 98, 128]
@@ -48,6 +48,8 @@ for i in flattened_list:
     expt_call = f"{base_call} "
     for j in range(len(i)):
         expt_call += f"--" + components[j] + f" {i[j]} "
+    if not i[1] == 0:
+        expt_call += f"--load_model"
     print(expt_call, file=output_file)
 #
 # for i in flattened_list:
