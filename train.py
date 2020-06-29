fda2f049b12c9eb30d537d0bbbc8bf4595514f4e
Will <will.hartley@epimorphics.com>
2020-06-29 14:54:33 +0100

    Check flushing

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ import os
 from util import util
 
 if __name__ == '__main__':
-    print('Python Starting Script')
+    print('Python Starting Script', flush=True)
     opt = TrainOptions().parse()
 
     # opt.dataroot = './dataset/SUN2012/%s/' % opt.phase
