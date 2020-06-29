7b78a4bd31448a0108de114a24c5392065ac4686
Will <will.hartley@epimorphics.com>
2020-06-29 11:06:30 +0100

    Going for a job

--- python_experiment_scripts/gen_vec_experiment.py
+++ python_experiment_scripts/gen_vec_experiment.py
@@ -17,7 +17,7 @@ cpoint_path = f'{SCRATCH_HOME}/checkpoints'
 
 base_call = f"python train.py --sample_p .1 --niter_decay 0 --display_id -1 " \
     f"--data_dir {sorted_path} --checkpoints_dir {cpoint_path} --phase train_partition " \
-    f"--batch_size 60 --gpu_ids 0,1,2,3,4,5,6,7 --lr 0.000001 --weighted_mask"
+    f"--batch_size 128 --gpu_ids 0,1,2,3,4,5,6,7 --lr 0.000001 --weighted_mask"
 # base_call2 = f"python train.py --name Auto --sample_p .125 --niter 20 --niter_decay 0 --lr 0.000001 --display_id -1 " \
 #     f"--data_dir {sorted_path} --checkpoints_dir {cpoint_path} --phase train --load_sg_model"
 
@@ -31,10 +31,10 @@ base_call = f"python train.py --sample_p .1 --niter_decay 0 --display_id -1 " \
 # weight_decays = [1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1]
 
 components = ['niter', 'epoch_count']
-niters = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
-epoch_counts = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]
+niters = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+epoch_counts = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 flattened_list  = list(zip(niters, epoch_counts))
-portion_vec = ['n014', 'n015', 'n016', 'n017', 'n018', 'n019', 'n020', 'n021', 'n022', 'n023']
+portion_vec = ['n01', 'n02', 'n03', 'n04', 'n05', 'n06', 'n07', 'n08', 'n09', 'n10', 'n1']
 assert len(flattened_list) == len(portion_vec)
 # print(flattened_list)
 # batch_sizes = [32, 64, 98, 128]
