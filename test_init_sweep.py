5c5c818805ddbbe822a3d5625d0111b9340bc6cc
Will <will.hartley@epimorphics.com>
2020-07-16 17:12:33 +0100

    Bug fix and weight init

--- test_init_sweep.py
+++ test_init_sweep.py
@@ -48,7 +48,7 @@ if __name__ == '__main__':
     #     # torch.manual_seed(5)
     #     # random.seed(5)
 
-    num_points = 10
+    # num_points = 10
     opt2 = copy.deepcopy(opt)
     opt2.weighted_mask = False
     opt2.spread_mask = False
@@ -92,7 +92,7 @@ if __name__ == '__main__':
                 data_raw[0] = data_raw[0].cuda()
             data_raw[0] = util.crop_mult(data_raw[0], mult=8)
 
-
+            num_points = np.random.randint(2, 30)
 
             data = util.get_colorization_data(data_raw, opt, ab_thresh=0., num_points=num_points)
             model.set_input(data)
@@ -115,8 +115,10 @@ if __name__ == '__main__':
             model2.set_input(data)
             model2.test()
             visuals2 = model2.get_current_visuals()
-            plt.imshow(util.tensor2im(visuals2['fake_reg']))
-            plt.show()
+
+            if opt.plot_data_gen:
+                plt.imshow(util.tensor2im(visuals2['fake_reg']))
+                plt.show()
 
             # just_ab_as_rgb_smoothed = apply_smoothing(data['abRgb'][nn, :, :, :], opt)
             just_ab_as_rgb_smoothed = util.apply_smoothing(visuals2['fake_reg'], opt)
@@ -124,8 +126,9 @@ if __name__ == '__main__':
             # labels = dbscan_encoded_indexed(ab_bins)
             labels, num_labels = util.bins_scimage_group_minimal(ab_bins)
 
-            plt.imshow(labels)
-            plt.show()
+            if opt.plot_data_gen:
+                plt.imshow(labels)
+                plt.show()
 
             # num_same_bin = len(labels[labels == unique_bins[0]])
             # total_size = data['A'].shape[2] * data['B'].shape[3]
@@ -167,38 +170,14 @@ if __name__ == '__main__':
             bar.update(i)
 
         # Save results
-        psnrs_mean = np.mean(psnrs, axis=0)
-        psnrs_std = np.std(psnrs, axis=0) / np.sqrt(opt.how_many)
 
-        # save_cpoint_dir = os.path.join(opt.checkpoints_dir, '%s/psnrs_mean_%s' % (opt.name,str_now))
-        np.save('%s%s/shifted_psnrs_mean_%s' % (opt.checkpoints_dir, opt.name,str_now), psnrs_mean)
-        np.save('%s%s/shifted_psnrs_std_%s' % (opt.checkpoints_dir, opt.name,str_now), psnrs_std)
-        np.save('%s%s/shifted_psnrs_%s' % (opt.checkpoints_dir, opt.name,str_now), psnrs)
-        psnrmeans = ['%.2f' % psnr for psnr in psnrs_mean]
-        print('PSNR Means: ', psnrmeans)
+        np.save('%s%s/init_psnrs_%s' % (opt.checkpoints_dir, opt.name,str_now), psnrs)
 
     else:
         str_now = '%02d_%02d_%02d%02d' % (7, 9, 12, 33)
-        psnrs_mean = np.load('%s%s/shifted_psnrs_mean_%s.npy' % (opt.checkpoints_dir, opt.name, str_now))
-        psnrs_std = np.load('%s%s/shifted_psnrs_std_%s.npy' % (opt.checkpoints_dir, opt.name,str_now))
-        psnrs = np.load('%s%s/shifted_psnrs_%s.npy' % (opt.checkpoints_dir, opt.name,str_now))
-        psnrmeans = ['%.2f' % psnr for psnr in psnrs_mean]
-        print('PSNR Means: ', psnrmeans)
+        psnrs = np.load('%s%s/init_psnrs_%s.npy' % (opt.checkpoints_dir, opt.name,str_now))
 
-
-
-    num_points_hack = 1. * num_points
-    # num_points_hack[0] = .4
-
-    plt.plot(randomisers, psnrs_mean, 'bo-', label=str_now)
-    plt.plot(randomisers, psnrs_mean + psnrs_std, 'b--')
-    plt.plot(randomisers, psnrs_mean - psnrs_std, 'b--')
-
-    # plt.xscale('log')
-    # plt.xticks([.4,1,2,5,10,20,50,100,200,500],
-    #     ['Auto','1','2','5','10','20','50','100','200','500'])
-    plt.xlabel('Random Shift')
-    plt.ylabel('PSNR [db]')
-    plt.legend(loc=0)
-    # plt.xlim((randomisers[0], randomisers[-1]))
-    plt.savefig('%s%s/shift_sweep_%s.png' % (opt.checkpoints_dir, opt.name, str_now))
+    psnrs_mean = np.mean(psnrs, axis=0)
+    psnrs_std = np.std(psnrs, axis=0) / np.sqrt(opt.how_many)
+    print(psnrs_mean)
+    print(psnrs_std)
